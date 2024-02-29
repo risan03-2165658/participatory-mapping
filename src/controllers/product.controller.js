ce656d9c0e24206bf4a81b8ae24a3d61034f8cd9
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 21:04:04 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,41 +1,52 @@
-
 const db = require("../config/database");
 
-const {Parser} = require('json2csv');
+const { Parser } = require('json2csv');
 const json2csvParser = new Parser();
 
+/**
+ * getRecord: Obtains all records
+ */
+exports.getRecord = async (req, res) => {
+    try {
+        const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');
+        const json = JSON.stringify(response);
+        res.status(200).send(json);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: "Error occurred while fetching records!",
+            error: error.message
+        });
+    }
+};
 
-// /**
-//  * getRecords: Obtains all records
-//  */
-// getRecords: Obtains all records
-exports.getRecord = async(req, res) => {//
-    const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');//
-    const json = JSON.stringify(response);
-    res.status(200).send(json);//
-};//
+/**
+ * addRecord: Inserts user insert data of review into tblRecord in the database
+ * @param {form} req - form body that contains user selected information
+ * @param {status} res - confirmation that comment has been added into the review table
+ */
+exports.addRecord = async (req, res) => {
+    try {
+        let { contributor, content, location, lat, lng } = req.body;
+        // let currTime = new Date().toISOString();
+        console.log('INSERT INTO "tblRecord"(contributor, content, location, lat, lng) VALUES ($1, $2, $3, $4, $5)',
+            [contributor, content, lat, lng]);
+        await db.query(
+            'INSERT INTO "tblRecord"(contributor, content, location, lat, lng) VALUES ($1, $2, $3, $4, $5)',
+            [contributor, content, location, lat, lng]
+        );
 
-
-
-// /**
-//  * Insert Comment/Review: Inserts user insert data of review into tblReview in the database
-//  * @param {form} req - form body that contains user selected information
-//  * @param {status} res - confirmation that comment has been added into the review table
-//  */
-exports.addRecord = async(req, res) => {
-    let {contributor, content, location, lat, lng} = req.body;
-    // let currTime = new Date().toISOString();
-    console.log('INSERT INTO "tblRecord"(contributor, content, location, lat, lng) VALUES ($1, $2, $3, $4, $5)',
-        [contributor, content, lat, lng]);
-    let {recordRows} = await db.query(
-        'INSERT INTO "tblRecord"(contributor, content, location, lat, lng) VALUES ($1, $2, $3, $4, $5)',
-        [contributor, content, location, lat, lng]
-    )
-
-    res.status(200).send({
-        message: "record added into record table!",
-        body: {
-            record: {contributor, content, location, lat, lng}
-        }
-    })
+        res.status(200).send({
+            message: "Record added into record table!",
+            body: {
+                record: { contributor, content, location, lat, lng }
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({
+            message: "Error occurred while adding record into record table!",
+            error: error.message
+        });
+    }
 };
