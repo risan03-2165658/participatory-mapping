de826407645765582c0d04097a8cd2e0e80d3dff
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 19:09:04 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,4 +1,22 @@
 const db = require("../config/database");
+const path = require("path");
+const fs = require("fs");
+
+// Define the directory where uploaded images will be stored
+const uploadDir = path.join(__dirname, "../uploads");
+
+// Create the directory if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
+// Function to handle file uploads
+const handleFileUpload = (file) => {
+    const fileName = `${Date.now()}-${file.name}`;
+    const filePath = path.join(uploadDir, fileName);
+    file.mv(filePath); // Move the uploaded file to the designated directory
+    return filePath;
+};
 
 // /**
 //  * getRecords: Obtains all records
@@ -24,7 +42,15 @@ exports.getRecord = async (req, res) => {
 //  */
 exports.addRecord = async (req, res) => {
     try {
-        let { contributor, content, location, image, lat, lng } = req.body;
+        let { contributor, content, location, lat, lng } = req.body;
+        let image = null;
+
+        // Check if an image file was uploaded
+        if (req.files && req.files.image) {
+            // Handle file upload and get the file path
+            image = handleFileUpload(req.files.image);
+        }
+
         console.log('INSERT INTO "tblRecord"(contributor, content, lat, lng, location, image) VALUES ($1, $2, $3, $4, $5, $6)',
             [contributor, content, lat, lng, location, image]);
 
@@ -36,7 +62,7 @@ exports.addRecord = async (req, res) => {
         res.status(200).send({
             message: "Record added into record table!",
             body: {
-                record: { contributor, content, lat, lng, location, image}
+                record: { contributor, content, lat, lng, location, image }
             }
         });
     } catch (error) {
