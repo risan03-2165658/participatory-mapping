211e8cc8112b0b58e663158d7842fc3d1ca04640
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 22:35:20 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,40 +1,35 @@
 const db = require("../config/database");
 
-const {Parser} = require('json2csv');
+const { Parser } = require('json2csv');
 const json2csvParser = new Parser();
 
-
-// /**
-//  * getRecords: Obtains all records
-//  */
-// getRecords: Obtains all records
+// Function to obtain all records
 exports.getRecord = async(req, res) => {//
     const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');//
     const json = JSON.stringify(response);
     res.status(200).send(json);//
 };//
 
+// Function to insert a new record
+exports.addRecord = async (req, res) => {
+    try {
+        let { contributor, place, content, lat, lng } = req.body;
+        console.log('INSERT INTO "tblRecord"(contributor, place, content, lat, lng) VALUES ($1, $2, $3, $4, $5)',
+            [contributor, place, content, lat, lng]);
+        let { recordRows } = await db.query(
+            'INSERT INTO "tblRecord"(contributor, place, content, lat, lng) VALUES ($1, $2, $3, $4, $5)',
+            [contributor, place, content, lat, lng]
+        );
 
-
-// /**
-//  * Insert Comment/Review: Inserts user insert data of review into tblReview in the database
-//  * @param {form} req - form body that contains user selected information
-//  * @param {status} res - confirmation that comment has been added into the review table
-//  */
-exports.addRecord = async(req, res) => {
-    let {contributor, content, lat, lng} = req.body;
-    // let currTime = new Date().toISOString();
-    console.log('INSERT INTO "tblRecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
-        [contributor, content, lat, lng]);
-    let {recordRows} = await db.query(
-        'INSERT INTO "tblRecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
-        [contributor, content, lat, lng]
-    )
-
-    res.status(200).send({
-        message: "record added into record table!",
-        body: {
-            record: {contributor, content, lat, lng}
-        }
-    })
+        res.status(200).send({
+            message: "Record added into record table!",
+            body: {
+                record: { contributor, place, content, lat, lng }
+            }
+        });
+    } catch (error) {
+        console.error("Error adding record:", error);
+        res.status(500).send("Internal Server Error");
+    }
 };
+
