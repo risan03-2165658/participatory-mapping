569b8511c1bc95a232f36fe53e95e5fe3495f5f5
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 17:15:49 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,33 +1,41 @@
+
 const db = require("../config/database");
+
+const {Parser} = require('json2csv');
+const json2csvParser = new Parser();
+
+
+// /**
+//  * getRecords: Obtains all records
+//  */
+// getRecords: Obtains all records
+exports.getRecord = async(req, res) => {//
+    const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');//
+    const json = JSON.stringify(response);
+    res.status(200).send(json);//
+};//
+
+
 
 // /**
 //  * Insert Comment/Review: Inserts user insert data of review into tblReview in the database
 //  * @param {form} req - form body that contains user selected information
 //  * @param {status} res - confirmation that comment has been added into the review table
 //  */
-exports.addRecord = async (req, res) => {
-    let { contributor, content, location, lat, lng, image } = req.body;
-    console.log('INSERT INTO "tblRecord"(contributor, content, location, lat, lng, image) VALUES ($1, $2, $3, $4, $5, $6)',
-        [contributor, content, location, lat, lng, image]);
+exports.addRecord = async(req, res) => {
+    let {contributor, content, lat, lng} = req.body;
+    // let currTime = new Date().toISOString();
+    console.log('INSERT INTO "tblRecord"(contributor, content, location, image, lat, lng) VALUES ($1, $2, $3, $4, $5, $6)',
+        [contributor, content, location, image, lat, lng]);
+    let {recordRows} = await db.query(
+        'INSERT INTO "tblRecord"(contributor, content, location, image, lat, lng) VALUES ($1, $2, $3, $4, $5, $6)',
+        [contributor, content, location, image, lat, lng]
+    )
 
-    try {
-        // Insert the record into the database
-        let { recordRows } = await db.query(
-            'INSERT INTO "tblRecord"(contributor, content, location, lat, lng, image) VALUES ($1, $2, $3, $4, $5, $6)',
-            [contributor, content, location, lat, lng, image]
-        );
-
-        res.status(200).send({
-            message: "Record added into record table!",
-            body: {
-                record: { contributor, content, location, lat, lng, image }
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({
-            message: "Error occurred while adding record into record table!",
-            error: error.message
-        });
-    }
+    res.status(200).send({
+        message: "record added into record table!",
+        body: {
+            record: {contributor, content, location, image, lat, lng}
+        }
+    })
 };
