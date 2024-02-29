b83bfa5386daf5fa011aace58fb00a23da629fee
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 17:02:43 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,40 +1,41 @@
 const db = require("../config/database");
-
-const {Parser} = require('json2csv');
+const { Parser } = require('json2csv');
 const json2csvParser = new Parser();
 
+/**
+ * getRecord: Obtains all records
+ */
+exports.getRecord = async (req, res) => {
+    try {
+        const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');
+        res.status(200).json(response.rows);
+    } catch (error) {
+        console.error("Error retrieving records:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
 
-// /**
-//  * getRecords: Obtains all records
-//  */
-// getRecords: Obtains all records
-exports.getRecord = async(req, res) => {//
-    const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');//
-    const json = JSON.stringify(response);
-    res.status(200).send(json);//
-};//
+/**
+ * addRecord: Inserts user insert data of review into tblRecord in the database
+ * @param {form} req - form body that contains user selected information
+ * @param {status} res - confirmation that comment has been added into the record table
+ */
+exports.addRecord = async (req, res) => {
+    try {
+        const { contributor, content, location, lat, lng } = req.body;
+        const image = req.file.filename; // Assuming the uploaded image filename is saved in req.file
 
+        const queryText = 'INSERT INTO "tblRecord"(contributor, content, location, lat, lng, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+        const values = [contributor, content, location, lat, lng, image];
 
+        const { rows } = await db.query(queryText, values);
 
-// /**
-//  * Insert Comment/Review: Inserts user insert data of review into tblReview in the database
-//  * @param {form} req - form body that contains user selected information
-//  * @param {status} res - confirmation that comment has been added into the review table
-//  */
-exports.addRecord = async(req, res) => {
-    let {contributor, content, lat, lng} = req.body;
-    // let currTime = new Date().toISOString();
-    console.log('INSERT INTO "tblRecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
-        [contributor, content, lat, lng]);
-    let {recordRows} = await db.query(
-        'INSERT INTO "tblRecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
-        [contributor, content, lat, lng]
-    )
-
-    res.status(200).send({
-        message: "record added into record table!",
-        body: {
-            record: {contributor, content, lat, lng}
-        }
-    })
+        res.status(201).json({
+            message: "Record added into record table!",
+            record: rows[0]
+        });
+    } catch (error) {
+        console.error("Error adding record:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
