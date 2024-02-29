789fc0eb1560ad8ac7dd9f52f6731f7abe1e43cc
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 17:08:33 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,41 +1,33 @@
 const db = require("../config/database");
-const { Parser } = require('json2csv');
-const json2csvParser = new Parser();
 
-/**
- * getRecord: Obtains all records
- */
-exports.getRecord = async (req, res) => {
-    try {
-        const response = await db.query('SELECT * FROM "tblRecord" ORDER BY id ASC');
-        res.status(200).json(response.rows);
-    } catch (error) {
-        console.error("Error retrieving records:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-};
-
-/**
- * addRecord: Inserts user insert data of review into tblRecord in the database
- * @param {form} req - form body that contains user selected information
- * @param {status} res - confirmation that comment has been added into the record table
- */
+// /**
+//  * Insert Comment/Review: Inserts user insert data of review into tblReview in the database
+//  * @param {form} req - form body that contains user selected information
+//  * @param {status} res - confirmation that comment has been added into the review table
+//  */
 exports.addRecord = async (req, res) => {
+    let { contributor, content, location, lat, lng, image } = req.body;
+    console.log('INSERT INTO "tblRecord"(contributor, content, location, lat, lng, image) VALUES ($1, $2, $3, $4, $5, $6)',
+        [contributor, content, location, lat, lng, image]);
+
     try {
-        const { contributor, content, location, lat, lng } = req.body;
-        const image = req.file.filename; // Assuming the uploaded image filename is saved in req.file
+        // Insert the record into the database
+        let { recordRows } = await db.query(
+            'INSERT INTO "tblRecord"(contributor, content, location, lat, lng, image) VALUES ($1, $2, $3, $4, $5, $6)',
+            [contributor, content, location, lat, lng, image]
+        );
 
-        const queryText = 'INSERT INTO "tblRecord"(contributor, content, location, lat, lng, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-        const values = [contributor, content, location, lat, lng, image];
-
-        const { rows } = await db.query(queryText, values);
-
-        res.status(201).json({
+        res.status(200).send({
             message: "Record added into record table!",
-            record: rows[0]
+            body: {
+                record: { contributor, content, location, lat, lng, image }
+            }
         });
     } catch (error) {
-        console.error("Error adding record:", error);
-        res.status(500).json({ error: "Internal server error" });
+        console.error(error);
+        res.status(500).send({
+            message: "Error occurred while adding record into record table!",
+            error: error.message
+        });
     }
 };
