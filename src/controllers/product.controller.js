a4a87a56be3cbe1759572443a84bcfaf03993f02
Risa <92127228+risan03-2165658@users.noreply.github.com>
2024-02-28 22:28:53 -0800

    Update product.controller.js

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -22,19 +22,19 @@ exports.getRecord = async(req, res) => {//
 //  * @param {status} res - confirmation that comment has been added into the review table
 //  */
 exports.addRecord = async(req, res) => {
-    let {contributor, place, content, lat, lng} = req.body;
+    let {contributor, content, lat, lng} = req.body;
     // let currTime = new Date().toISOString();
-    console.log('INSERT INTO "tblRecord"(contributor, place, content, lat, lng) VALUES ($1, $2, $3, $4, $5)',
-        [contributor, place, content, lat, lng]);
+    console.log('INSERT INTO "tblRecord"(contributor, content, lat, lng) VALUES ($1, $2, $3, $4)',
+        [contributor, content, lat, lng]);
     let {recordRows} = await db.query(
-        'INSERT INTO "tblRecord"(contributor, place, content, lat, lng) VALUES ($1, $2, $3, $4, $5)',
-        [contributor, place, content, lat, lng]
+        'INSERT INTO "tblRecord"(contributor, place, content, lat, lng) VALUES ($1, $2, $3, $4)',
+        [contributor, content, lat, lng]
     )
 
     res.status(200).send({
         message: "record added into record table!",
         body: {
-            record: {contributor, place, content, lat, lng}
+            record: {contributor,content, lat, lng}
         }
     })
 };
